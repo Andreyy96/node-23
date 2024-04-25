a1b22ad27f4d0901db15b132e145ec68aec680cf
Andreyy96 <taurussilver777@gmail.com>
2024-04-25 17:37:44 +0300

    fix task5

--- src/middlewares/common.middleware.ts
+++ src/middlewares/common.middleware.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { isObjectIdOrHexString } from "mongoose";
 
 import { ApiError } from "../api-error";
+import { schemaForPostUser, schemaForPutUser } from "../user.validator";
 
 class CommonMiddleware {
   public isIdValid(req: Request, res: Response, next: NextFunction) {
@@ -9,6 +10,21 @@ class CommonMiddleware {
       const userId = req.params.id;
       if (!isObjectIdOrHexString(userId)) {
         throw new ApiError("Invalid id", 400);
+      }
+      next();
+    } catch (e) {
+      next(e);
+    }
+  }
+
+  public async isReqBodyValid(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { error } =
+        req.method === "POST"
+          ? schemaForPostUser.validate(req.body)
+          : schemaForPutUser.validate(req.body);
+      if (error) {
+        throw new ApiError(`${error.details[0].message}`, 400);
       }
       next();
     } catch (e) {
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -2,21 +2,19 @@ import { Router } from "express";
 
 import { userController } from "../controllers/user.controller";
 import { commonMiddleware } from "../middlewares/common.middleware";
-import { validReqPostMiddleware } from "../middlewares/valid-req-post.middleware";
-import { validReqPutMiddleware } from "../middlewares/valid-req-put.middleware";
 
 const router = Router();
 
 router.get("/", userController.getList);
 
-router.post("/", validReqPostMiddleware.isIdValidReq, userController.create);
+router.post("/", commonMiddleware.isReqBodyValid, userController.create);
 
 router.get("/:id", commonMiddleware.isIdValid, userController.getUser);
 
 router.put(
   "/:id",
   commonMiddleware.isIdValid,
-  validReqPutMiddleware.isIdValidReq,
+  commonMiddleware.isReqBodyValid,
   userController.update,
 );
 
