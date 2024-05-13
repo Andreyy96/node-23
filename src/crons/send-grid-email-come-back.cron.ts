416305814ecbacfc41e3ee0779dc158138872322
Andreyy96 <taurussilver777@gmail.com>
2024-05-13 17:45:00 +0300

    fix task9

--- src/crons/send-grid-email-come-back.cron.ts
+++ src/crons/send-grid-email-come-back.cron.ts
@@ -1,28 +1,30 @@
 import { CronJob } from "cron";
 
 import { EmailTypeEnum } from "../enums/email-type.enum";
-import { tokenRepository } from "../repositories/token.repository";
+import { TimeHelper } from "../helpers/time.helper";
 import { userRepository } from "../repositories/user.repository";
 import { sendGridService } from "../services/send-grid.service";
 
 const handler = async () => {
   try {
-    console.log("[START CRON] Send email");
-    const users = await userRepository.getList();
-
-    for (const user of users) {
-      const tokens = await tokenRepository.findByParams({ _userId: user._id });
-      if (!tokens) {
-        await sendGridService.sendByType(user.email, EmailTypeEnum.COME_BACK, {
-          name: user.name,
-        });
-      }
-    }
-    return;
-  } catch (e) {
-    console.error("sendEmail", e);
+    console.log("[START CRON] send email");
+    const date = TimeHelper.subtractByParams(1, "day");
+    const users = await userRepository.findWithOutActivityAfter(date);
+    await Promise.all(
+      users.map(async (user) => {
+        console.log(user);
+        return await sendGridService.sendByType(
+          user.email,
+          EmailTypeEnum.COME_BACK,
+          { name: user.name },
+        );
+      }),
+    );
+  } catch (error) {
+    console.error("notifyOldVisitors: ", error);
   } finally {
-    console.log("[END CRON] Send email");
+    console.log("[END CRON] send email");
   }
 };
-export const sendComeBackEmail = new CronJob("0 0 12 * * 6", handler);
+
+export const sendComeBackEmail = new CronJob("* * * 4 * *", handler);
